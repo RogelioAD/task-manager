93dee2f0f71d4e0e0fad607fb30b02f600acd529
Rogelio Acosta <rogeracosta2001@gmail.com>
2025-07-12 14:14:23 -0700

    first commit

---
+++ server-side/models/user.ts
@@ -0,0 +1,55 @@
+import { DataTypes, Model, Sequelize } from 'sequelize';
+
+export class User extends Model {
+    declare id: number;
+    declare username: string;
+    declare firstName: string;
+    declare lastName: string;
+    declare email: string;
+    declare passwordHash: string;
+    declare readonly createdAt: Date;
+    declare readonly updatedAt: Date;
+    declare readonly deletedAt: Date | null;
+}
+
+export function initUser(sequelize: Sequelize) {
+    User.init({
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+        },
+        firstName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        lastName: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true,
+            },
+        },
+        passwordHash: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    }, {
+        sequelize,
+        modelName: 'User',
+        tableName: 'users',
+        timestamps: true,
+        paranoid: true,
+    });
+    return User;
+}
